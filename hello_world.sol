c03830ee8da06ade3b6ae446e768869ea9858f14
Jack Antico <you@example.com>
2022-03-09 22:12:32 -0500

    First commit

---
+++ hello_world.sol
@@ -0,0 +1,21 @@
+
+pragma solidity ^0.7.3;
+
+
+
+
+contract HelloWorld {
+
+   event UpdatedMessages(string oldStr, string newStr);
+   string public message;
+
+   function update(string memory newMessage) public {
+      string memory oldMsg = message;
+      message = newMessage;
+      emit UpdatedMessages(oldMsg, newMessage);
+   }
+
+   function return_message() public view returns(string msg){
+     return message;
+   }
+}
